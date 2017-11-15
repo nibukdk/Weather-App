63c71be5c4af72575da0fcbe89e38a50e13c419e
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-11-15 16:36:50 +0200

    Addition of materialize css and removing bootstrap

--- weather.js
+++ weather.js
@@ -10,17 +10,17 @@ var dateToday= date.getUTCDate()+'.'+date.getUTCMonth()+'.'+date.getUTCFullYear(
 var day= dayNames[date.getUTCDay()];
 
 
-	
-	
+
+
 		$.getJSON("http://ip-api.com/json/?callback=?",function(data){
 			lattitude=data.lat;
 			longitude=data.lon;
 		 	city=data.city;
-			
+
 			$('.city-name').html(city);
 			$('.dayOfWeek').html(day);
 			$('.fullDate').html(dateToday);
-		
+
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lattitude+"&lon="+longitude+"&appid=91cd24753027284fc8d71cfa2c102253",function(data){
 			var weather = data.weather[0].main,
 			 	temp = data.main.temp,
@@ -41,9 +41,9 @@ var day= dayNames[date.getUTCDay()];
 			 	$('#minTemp').html(minTempinCelcius);
 
 			var weatherArray=[weather,temp,maxTemp,minTemp,windSpeed];
-				
-				
-				
+
+
+
 				$('input[type=checkbox]').on('click',function(){
 					if(this.checked){
 						$('#temp').html(inFarenheit+"F");
@@ -58,11 +58,11 @@ var day= dayNames[date.getUTCDay()];
 					}
 
 				});
-			
+
 				$('#weather').html(weather);
 				$('.windSpeed').append('<br>'+windSpeed+'m/s');
 				$('h3').html(weatherCondition);
-				
+
 				if(weather==='Clear'){
 					$('.thumbnail').html('<p> <i class="fa fa-sun-o fa-5x" aria-hidden="true"></i> </p>');
 				}
@@ -75,17 +75,13 @@ var day= dayNames[date.getUTCDay()];
 				else if(weather==='Thunderstorm'){
 					$('.thumbnail').html('<p> <i class="fa fa-bolt fa-5x" aria-hidden="true"></i>  </p>');
 				}
-				
+
 				else if (weather==='Clouds'){
 					$('.thumbnail').html('<p> <i class="fa fa-cloud fa-5x" aria-hidden="true"></i> </p>');
 				}
 				else{
 					$('.fa').addClass('.fa');
 				}
-							
+
 		});
 	});
-
-
-					
-	
